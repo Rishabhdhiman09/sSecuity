8c623fd0b4ebf7f1d0e026b9c696593b4492a7d6
rishabhdhiman09 <rishabhdhiman09@gmail.com>
2021-07-18 20:40:03 +0530

    ant matchers

--- src/main/java/com/example/demo/config/MySecurityConfig.java
+++ src/main/java/com/example/demo/config/MySecurityConfig.java
@@ -1,9 +1,16 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,9 +19,25 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/public/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    // user defined username and password
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("rishabh").password(this.passwordEncoder().encode("rrrrr")).roles("NORMAL");
+        auth.inMemoryAuthentication().withUser("rahul").password(this.passwordEncoder().encode("rrrrr")).roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10); // it is preferred
     }
 }
