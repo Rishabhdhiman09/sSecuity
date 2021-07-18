537660207f2677f656f33847607ab10463175edf
rishabhdhiman09 <rishabhdhiman09@gmail.com>
2021-07-18 23:22:28 +0530

    CSRF done

--- src/main/java/com/example/demo/config/MySecurityConfig.java
+++ src/main/java/com/example/demo/config/MySecurityConfig.java
@@ -20,6 +20,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
 //                .antMatchers("/public/**").hasRole("NORMAL")
                 .anyRequest()
